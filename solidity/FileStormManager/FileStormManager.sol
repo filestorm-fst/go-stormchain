d8172e7c955d859e7d904a2deb9c995d7c255bbd
longjinwen <396983001@qq.com>
2020-05-26 20:44:14 -0700

    updated FileStormManager.sol

--- solidity/FileStormManager/FileStormManager.sol
+++ solidity/FileStormManager/FileStormManager.sol
@@ -1,146 +1,131 @@
-pragma solidity ^0.5.2;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
 /**
  * @title FileStormManager.sol
  * This is the smart contract to manage filestorm nodes on a blockchain.
  */
 
-contract FileStormManager {
+import "ReentrancyGuard.sol";
+
+contract FileStormManager is ReentrancyGuard {
 
     address internal owner;
     mapping(address => uint) public admins;
 
     uint256 public BLOCK_SECOND = 5;
     uint256 public DISBURSE_EPOCH = 720 * 24; //once a day
-    uint256 public staking_amount = 1000 * 10 ** 18;
-    uint256 public disburse_amount = 8 * 10 ** 17;
+    uint256 public DISBURSE_TOTAL = 400; // disburse only 400 times. 
+    uint256 public staking_amount = 500 * 10 ** 18; // staking amount per terabyte.
+    uint256 public staking_limit = 96; // one machine can only store 96TB at most. 
 
-    // create a parameter for withdraw wait block
-    // create mapping of nodes, staking amount, total disbursement, ..., time/block, finish block
+    // add limit for each register type
+    enum RegisterType {consensus, store, retrieval, instrastructure}
 
-    //total disbursement
-    uint256 constant public totalDisbursement = 2 * 10 ** 8; //200 million
+    mapping(uint => uint256) public disburseMapping;
 
-    //mapping of nodes in network
-    mapping(address => bool) nodes;
+    enum NodeStatus {registered, working, error, ended}
 
-    //mapping of staking amount for each node in network
-    mapping(address => uint256) stakingAmount;
+    struct Node {
+      string  nodeId;
+      address nodeAddress;
+      address beneficiary;
+      uint256 stakingAmount;
+      uint256 disburseAmount;
+      uint256 registerBlock;
+      uint registerType;
+      uint nodeStatus; 
+      uint256 nextRewardBlock;
+      uint256 disburseCount;
+      uint256 disbursedTotal;
+    }
 
-    //mapping for each address to their filestorm ID
-    mapping(string => address) stormIDOwner;
-
-    //mapping for finish block for each removed node
-    mapping(address => uint256) finishBlock;
-
-    //array to keep track of all nodes to be removed
-    address[] nodeRemovals;
-
-    //array of all nodes to be disbursed
-    address[] disburseNodes;
+    mapping(address => Node) public nodeMapping;
 
     constructor() public payable {
-        owner = msg.sender;
-        admins[msg.sender] = 1;
+      owner = msg.sender;
+      admins[msg.sender] = 1;
+      
+      disburseMapping[uint(RegisterType.consensus)] = 25 * 10 ** 17;
+      disburseMapping[uint(RegisterType.retrieval)] = 35 * 10 ** 17;
+      disburseMapping[uint(RegisterType.store)] = 375 * 10 ** 16;
+      disburseMapping[uint(RegisterType.instrastructure)] = 4 * 10 ** 18;
     }
 
     function addAdmin(address admin) public {
-        require(admins[msg.sender] == 1, "Only Admins Can Add Another Admin.");
-        admins[admin] = 1;
+      require(admins[msg.sender] == 1, "Only Admins Can Add Another Admin.");
+      admins[admin] = 1;
     }
 
     function removeAdmin(address admin) public {
-        require(admins[msg.sender] == 1, "Only Admins Can Remove Another Admin.");
-        require(admin != msg.sender, "Admins Cannot Remove Self.");
-        admins[admin] = 0;
+      require(admins[msg.sender] == 1, "Only Admins Can Remove Another Admin.");
+      require(admin != msg.sender, "Admins Cannot Remove Self.");
+      admins[admin] = 0;
     }
 
     function updateStakingAmount(uint256 amount) public {
-        require(admins[msg.sender] == 1, "Only Admins Can Change Staking Amount.");
-        staking_amount = amount;
+      require(admins[msg.sender] == 1, "Only Admins Can Change Staking Amount.");
+      staking_amount = amount;
     }
 
-    function updateDisburseAmount(uint256 amount) public {
-        require(admins[msg.sender] == 1, "Only Admins Can Change Disburse Amount.");
-        disburse_amount = amount;
+    function updateStakingLimit(uint256 limit) public {
+      require(admins[msg.sender] == 1, "Only Admins Can Change Staking Limit.");
+      staking_limit = limit;
     }
 
-    function addNode(address nodeAddress, string memory fileStormId) payable public {
-        //make sure staking amount is greater than the minimum
-        require(msg.value >= staking_amount, "Staking amount not high enough");
-
-        // token amount should be multiple of 1000
-        require(msg.value % 1000 == 0, "Token Amount Should Be A Multiple Of 1000.");
-
-        // addNode to global node mapping
-        nodes[nodeAddress] = true;
-        stakingAmount[nodeAddress] = msg.value;
-        stormIDOwner[fileStormId] = nodeAddress;
-        disburseNodes.push(nodeAddress);
-
+    function updateDisburseAmount(uint registerType, uint256 amount) public {
+      require(admins[msg.sender] == 1, "Only Admins Can Change Disburse Amount.");
+      disburseMapping[registerType] = amount;
     }
 
-    function removeNode(address nodeAddress, string memory fileStormId) public {
-        // check if it's the owner
-        require(nodeAddress == msg.sender, "Only owner of node can remove node");
-        require(nodeAddress == stormIDOwner[fileStormId], "FileStormId does not match the nodeAddress.");
+    function addNode(string memory nodeId, address nodeAddress, address beneficiary, uint256 stakingCount, uint registerType) public payable {
 
-        // remove node from mapping
-        delete nodes[nodeAddress]; //remove from nodes in network
-        delete stormIDOwner[fileStormId]; //remove stormID from network
+      require(admins[msg.sender] == 1 || msg.sender == nodeAddress || msg.sender == beneficiary
+      , "Only Admins or Node Owners Can Register a Node.");
 
-        // wait withdraw block to remove and give back tokens.
-        nodeRemovals.push(nodeAddress); //add to array and disburse funcion will take care of releasing stake ammount
-        finishBlock[nodeAddress] = block.number + DISBURSE_EPOCH;
+      require(nodeMapping[nodeAddress].nodeStatus == 0 || nodeMapping[nodeAddress].nodeStatus != uint(NodeStatus.ended)
+      , "Node is already registered.");
+      
+      require(disburseMapping[registerType]>0, "Wrong registration type.");
+      require(stakingCount <= staking_limit, "staking total reached limit.");
+
+      nodeMapping[nodeAddress].nodeId = nodeId;
+      nodeMapping[nodeAddress].nodeAddress = nodeAddress;
+      nodeMapping[nodeAddress].beneficiary = beneficiary;
+      nodeMapping[nodeAddress].stakingAmount = staking_amount * stakingCount;
+      nodeMapping[nodeAddress].disburseAmount = disburseMapping[registerType] * stakingCount;
+      nodeMapping[nodeAddress].registerType = registerType;
+      nodeMapping[nodeAddress].registerBlock = block.number;
+      nodeMapping[nodeAddress].nodeStatus = uint(NodeStatus.registered);
+      nodeMapping[nodeAddress].nextRewardBlock = block.number + DISBURSE_EPOCH;
+      nodeMapping[nodeAddress].disburseCount = 0;
+      nodeMapping[nodeAddress].disbursedTotal = 0;
+      
     }
 
-    function disburse() public {
-        // 200 million tokens in total mined in 30 years
-        // disburse evenly to all participating nodes based on how much the put in stake
-        uint256 nodeAmount = disburseNodes.length;
-        for(uint256 i = 0; i < nodeAmount; i++)
-        {
-          address payable temp = address(uint160(disburseNodes[i]));
-          temp.transfer(disburse_amount);
-        }
-
-        // remove node based on finish block number
-        uint256 currentBlock = block.number;
-        uint256 length = nodeRemovals.length;
-        for(uint256 i = 0; i < length; i++)
-        {
-          address removeAddress = nodeRemovals[i];
-          if(finishBlock[removeAddress] >= currentBlock)
-          {
-            //give back stake
-            address payable temp = address(uint160(removeAddress));
-            temp.transfer(stakingAmount[removeAddress]);
-            //remove from array
-            delete nodeRemovals[i];
-            delete stakingAmount[removeAddress];
-            delete finishBlock[removeAddress];
-          }
-        }
+    function updateNodeStatus(address nodeAddress, uint nodeStatus) public {
+      require(admins[msg.sender] == 1, "Only Admins Can Update Node Status.");
+      nodeMapping[nodeAddress].nodeStatus = nodeStatus;
     }
 
-    //added functions for testing purposes
-    function isNode(address node) public view returns (bool)
-    {
-      return nodes[node];
-    }
 
-    function nodeStakeAmount(address node) public view returns (uint256)
-    {
-      return stakingAmount[node];
-    }
+    function disburse(address nodeAddress) nonReentrant public {
+      require(admins[msg.sender] == 1 || msg.sender == nodeAddress || msg.sender == nodeMapping[nodeAddress].beneficiary
+      , "Only Admins or Node Owners Can Request Disbursement.");
 
-    function isAdmin(address addr) public view returns (bool)
-    {
-      if(admins[addr] == 1) return true;
-      return false;
-    }
+      require(nodeMapping[nodeAddress].nextRewardBlock <= block.number
+      , "Next Disbursement block not reached.");
 
-    function updateDisburseEpoch(uint256 time) public
-    {
-      DISBURSE_EPOCH = time;
+      if (nodeMapping[nodeAddress].nodeStatus == uint(NodeStatus.working)) {
+        nodeMapping[nodeAddress].beneficiary.call{value: nodeMapping[nodeAddress].disburseAmount};
+      }
+      nodeMapping[nodeAddress].nextRewardBlock = block.number + DISBURSE_EPOCH;
+      nodeMapping[nodeAddress].disburseCount ++;
+      nodeMapping[nodeAddress].disbursedTotal += nodeMapping[nodeAddress].disburseAmount;
+
+      if (nodeMapping[nodeAddress].disburseCount == DISBURSE_TOTAL) {
+        nodeMapping[nodeAddress].nodeStatus = uint(NodeStatus.ended);
+      }
     }
+    
 }
